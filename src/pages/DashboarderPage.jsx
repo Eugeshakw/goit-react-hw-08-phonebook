dd02e27dfa6fb7fb077419faa8347f525a45d5ed
Eugeshakw <eugeshakw@gmail.com>
2023-09-10 17:31:37 +0300

    update

--- src/pages/DashboarderPage.jsx
+++ src/pages/DashboarderPage.jsx
@@ -1,7 +1,7 @@
-import { useLogoutRedirect } from 'hooks/useLogoutRedirect';
+
 import { Form } from '../components/AllForm/AllForm';
 export const Dashboard = () => {
-  // useLogoutRedirect();
+  
 
   return (
     <>
